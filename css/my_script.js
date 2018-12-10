017447b971f96b216cd91f0e39be264e6103c102
kiran <kumar.alg2@gmail.com>
2018-12-10 18:56:09 +0530

    Initial commit

---
+++ css/my_script.js
@@ -0,0 +1,11 @@
+$(document).ready( function() {
+
+ $("#add_li").click( function() {
+    $("ol").append("<li></li>");
+ });
+ 
+  $("#remove_li").click( function() {
+    $("li:last").remove();
+ });
+
+});
